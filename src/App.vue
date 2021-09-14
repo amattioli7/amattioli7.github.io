f014e168e09a2a5f9a9f44ae4a248c7c65af4290
amattioli7 <afm45@pitt.edu>
2021-09-14 16:43:19 -0400

    Playing around with App Bar

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,51 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="black"
+      dense
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar-nav-icon @click="drawer = true">
+
+        <v-navigation-drawer
+          v-model="drawer"
+        >
+
+        <v-list nav>
+          <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+        </v-list>
+
+        </v-navigation-drawer>
+
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title>Adam Mattioli's Personal Website</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/amattioli7"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Check out my GitHub Repository!</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
