98deaed018e2e269ec4ab79d29b707b0f5d5eaaa
amattioli7 <afm45@pitt.edu>
2021-09-14 16:19:21 -0400

    Fixing Vue Repo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
+    <h1>Welcome to my Vue Website!</h1>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
