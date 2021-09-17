da578add54d4d7f7024e575cadf1afc87062cf85
amattioli7 <afm45@pitt.edu>
2021-09-17 14:49:47 -0400

    Added Contact Form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,56 @@
 <template>
+<v-layout
+align-center
+justify-center
+>
   <div class="contact">
-    <h1>Contact Me:</h1>
+    <h1 class="mt-4">Contact Me:</h1>
+
+    <v-spacer class="mb-4"></v-spacer>
+
+    <v-card
+      ref="form"
+      min-width="600px"
+    >
+
+      <v-card-text>
+        <v-text-field
+        ref="name"
+        placeholder="Name"
+        clearable
+        clear-icon="mdi-close-circle"
+        >
+        </v-text-field>
+
+        <v-text-field
+        ref="email"
+        placeholder="Email Address"
+        clearable
+        clear-icon="mdi-close-circle"
+        >
+        </v-text-field>
+
+        <v-text-field
+        ref="subject"
+        placeholder="Subject"
+        clearable
+        clear-icon="mdi-close-circle"
+        >
+        </v-text-field>
+
+        <v-textarea
+        ref="message"
+        placeholder="Message"
+        maxlength="500"
+        clearable
+        clear-icon="mdi-close-circle"
+        >
+        </v-textarea>
+
+      </v-card-text>
+        
+    </v-card>
+
   </div>
+</v-layout>
 </template>
